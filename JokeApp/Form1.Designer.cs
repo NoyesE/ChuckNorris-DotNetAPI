e812eb0e5f090162af81286d04e8e7c028f72dd4
Ian Noyes <noyese18@hotmail.com>
2019-04-15 16:20:17 -0700

    Form load adds categories to cmb box Get joke button fills txt box with a random joke

---
+++ JokeApp/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace JokeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetJoke = new System.Windows.Forms.Button();
+            this.txtJoke = new System.Windows.Forms.TextBox();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetJoke
+            // 
+            this.btnGetJoke.Location = new System.Drawing.Point(321, 230);
+            this.btnGetJoke.Name = "btnGetJoke";
+            this.btnGetJoke.Size = new System.Drawing.Size(75, 23);
+            this.btnGetJoke.TabIndex = 0;
+            this.btnGetJoke.Text = "Get Joke";
+            this.btnGetJoke.UseVisualStyleBackColor = true;
+            this.btnGetJoke.Click += new System.EventHandler(this.btnGetJoke_Click);
+            // 
+            // txtJoke
+            // 
+            this.txtJoke.Location = new System.Drawing.Point(214, 159);
+            this.txtJoke.Multiline = true;
+            this.txtJoke.Name = "txtJoke";
+            this.txtJoke.Size = new System.Drawing.Size(288, 65);
+            this.txtJoke.TabIndex = 1;
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(303, 112);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategory.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(303, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Categories";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.txtJoke);
+            this.Controls.Add(this.btnGetJoke);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetJoke;
+        private System.Windows.Forms.TextBox txtJoke;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JokeApp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChuckNorrisAPI;
+
+
+namespace JokeApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGetJoke_Click(object sender, EventArgs e)
+        {
+            Joke j = await ChuckNorrisClient.GetRandomJoke();
+            txtJoke.Text = j.JokeText;
+            
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            IEnumerable<string> categories = await ChuckNorrisClient.GetCategories();
+            foreach (var cat in categories)
+            {
+                cmbCategory.Items.Add(cat);
+            }
+                
+        }
+    }
+}
